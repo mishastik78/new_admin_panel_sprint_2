855b264bc0c6d139ac723acf6c0da33c4c45498e
mishastik78 <mishastik78@yande.ru>
2022-03-30 12:15:54 +0300

    ready

---
+++ 02_django_api/config/settings.py
@@ -0,0 +1,54 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from split_settings.tools import include
+
+load_dotenv()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DEBUG', False)  == 'True'
+
+
+include(
+    'components/allowed_hosts.py',
+    'components/installed_apps.py',
+    'components/middleware.py',
+    'components/templates.py',
+    'components/database.py',
+    'components/auth_pass_valid.py',
+    'components/internations.py',
+    'components/static_media.py',
+)
+
+ROOT_URLCONF = 'config.urls'
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOCALE_PATH = ['movies/locale']
+
+# Toolbar settings
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
+}
